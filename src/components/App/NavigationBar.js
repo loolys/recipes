eb29888f6f7eef7b36c4aff60ac61df4a349d4e6
loolys <pierre.liljekvist@outlook.com>
2017-02-28 12:31:08 +0100

    renders links on navbar conditionally depending on auth, and can logout.

--- src/components/App/NavigationBar.js
+++ src/components/App/NavigationBar.js
@@ -2,9 +2,32 @@ import React, { Component } from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router';
 import { LinkContainer } from 'react-router-bootstrap';
+import { connect } from 'react-redux';
+import { logout } from '../../actions/authActions';
 
 class NavigationBar extends Component {
+  logout(e) {
+    e.preventDefault();
+    this.props.logout();
+  }
   render() {
+    const userLinks = (
+      <Nav pullRight>
+        <NavItem href="#" onClick={this.logout.bind(this)}>Logout</NavItem>
+      </Nav>
+    );
+
+  const guestLinks = (
+    <Nav pullRight>
+      <LinkContainer to="/Signup">
+        <NavItem>Signup</NavItem>
+      </LinkContainer>
+      <LinkContainer to="/Login">
+        <NavItem>Login</NavItem>
+      </LinkContainer>
+    </Nav>
+  );
+    const { isAuthenticated } = this.props.auth;
     return (
       <Navbar>
         <Navbar.Header>
@@ -12,16 +35,21 @@ class NavigationBar extends Component {
           <Link to="/">React-Bootstrap</Link>
         </Navbar.Brand>
         </Navbar.Header>
-        <Nav pullRight>
-          <LinkContainer to="/Signup">
-            <NavItem>Signup</NavItem>
-          </LinkContainer>
-          <LinkContainer to="/Login">
-            <NavItem>Login</NavItem>
-          </LinkContainer>
-        </Nav>
+        { isAuthenticated ? userLinks : guestLinks }
       </Navbar>
     );
   }
 }
-export default NavigationBar;
+
+NavigationBar.propTypes = {
+  auth: React.PropTypes.object.isRequired,
+  logout: React.PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth,
+  };
+}
+
+export default connect(mapStateToProps, { logout })(NavigationBar);
