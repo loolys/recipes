c6e29a4477d22855a82e75a00beb8d19dab846a1
Pierre <pierre.liljekvist@outlook.com>
2017-03-11 08:02:49 +0100

    Added line through text on click for shopping list items and fixed the link for latests recipes on frontpage.

--- src/components/IndexPage/LatestRecipes.js
+++ src/components/IndexPage/LatestRecipes.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Col, Thumbnail } from 'react-bootstrap';
+import { Link } from 'react-router';
 
 class LatestRecipes extends React.Component {
   render() {
     console.log(this.props.data);
     const thumbnails = this.props.data.map(item => {
       return (<Col key={item._id} xs={6} md={3} sm={4}>
-        <Thumbnail src={item.image} alt="food">
-          <h3>{item.title}</h3>
-          <p>{item.description}</p>
-        </Thumbnail>
+        <Link to={`/recipe/${item._id}`}>
+          <Thumbnail src={item.image} alt="food">
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+          </Thumbnail>
+        </Link>
       </Col>);
     })
     return (
--- src/components/Profile/ShoppingList.js
+++ src/components/Profile/ShoppingList.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import classnames from 'classnames';
+import _ from 'lodash';
+
+import './ShoppingList.css';
 
 export default class ShoppingList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lineThrough : [],
+    }
+  }
+
+  componentWillMount() {
+    const line = this.props.ingredients.map(item => {
+      return {item: false}
+    });
+    this.setState({ lineThrough: line });
+  }
+
+  lineThrough = (index) => (evt) => {
+    let updateOrder = _.clone(this.state.lineThrough);
+    updateOrder[index].item = !this.state.lineThrough[index].item;
+    this.setState({ lineThrough: updateOrder });
+  }
   render() {
-    const shoppingList = this.props.ingredients.map(item => {
+    console.log(this.state.lineThrough[0].item);
+    const shoppingList = this.props.ingredients.map((item, index) => {
       return (
-        <div key={item.ingredient}>
-          <ListGroupItem>
+        <div
+          key={item.ingredient}
+          onClick={this.lineThrough(index)}
+          className={classnames('', { 'line-through': this.state.lineThrough[index].item})}
+        >
+          <ListGroupItem
+            name={item.ingredient}
+          >
             {item.amount + ' ' + item.unit + ' ' + item.ingredient}
           </ListGroupItem>
         </div>
