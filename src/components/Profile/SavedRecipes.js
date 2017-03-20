b047b0aa19811008e3fe2cc6caede84d3ca789a2
Pierre <pierre.liljekvist@outlook.com>
2017-03-20 10:59:24 +0100

    made shoppinglist wider on small screens

--- src/components/Profile/SavedRecipes.js
+++ src/components/Profile/SavedRecipes.js
@@ -92,7 +92,7 @@ class SavedRecipes extends React.Component {
     });
 
     return (
-      <div className="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
+      <div className="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-12">
         <Button onClick={this.toggleShoppingList} bsStyle="primary">
           Show Shopping List
         </Button>
